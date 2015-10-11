a371a92ce9f34032a80ca1c54a74687228da629c
vision <srikanthparupati@Srikanths-MacBook-Pro-2.local>
2015-10-11 15:54:37 -0400

    Added control logic to switch board and leader board logic

---
+++ GrabAWord/client/LeaderBoard.jsx
@@ -0,0 +1,21 @@
+LeaderBoard = React.createClass({
+  
+  mixins: [ReactMeteorData],
+
+  propTypes: {
+    
+  },
+
+  getMeteorData() {
+    return { };
+  },
+  
+  render() {
+    return (
+      <div>
+        <label> Leader board information </label>
+      </div>
+      );
+  }  
+
+});
