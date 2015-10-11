0f636f48a954cd21930050624f5efd6cb58d9b81
tekstop <tekstop@outlook.com>
2015-10-11 00:20:59 -0400

    moving css and adding basic method

--- GrabAWord/server/DictionaryMethods.jsx
+++ GrabAWord/server/DictionaryMethods.jsx
@@ -2,31 +2,37 @@ Meteor.methods({
 
   // Assuming we are passed a tileArray
   // Each tile has 2 properties - letter and value
-  allWordsInGame(tileArray) {
-    //create a string with the tileArray
-    let letters = "";
-    tileArray.forEach(tile => {
-      letters += tile.character;
+  // Cant use takes too much memory
+  allWordsInGame: function(letters) {
+    
+    let wordsArray = tree(letters.split('')).map(function(str) {
+      return str.join('')
     });
-
-    // permutation of letters
-    var tree = function(leafs) {
-      var branches = [];
-      if (leafs.length == 1) return leafs;
-      for (var k in leafs) {
-        var leaf = leafs[k];
-        tree(leafs.join('').replace(leaf, '').split('')).concat("").map(
-          function(subtree) {
-            branches.push([leaf].concat(subtree));
-          });
-      }
-      return branches;
-    };
-
-    let wordsArray = tree(letter.split('')).map(function(str){return str.join('')});
     
     // Find all the words in the dictionary
+    let gameDict = Words.find({key: {$in: wordsArray}}).count();
+    return gameDict;
 
+  },
+
+  isWordPresent: function(word) {
+  	if (Words.findOne({key:word})) {
+  		return true;
+  	}
+  	return false;
   }
 
 });
+
+var tree = function(leafs) {
+  var branches = [];
+  if (leafs.length == 1) return leafs;
+  for (var k in leafs) {
+    var leaf = leafs[k];
+    tree(leafs.join('').replace(leaf, '').split('')).concat("").map(
+      function(subtree) {
+        branches.push([leaf].concat(subtree));
+      });
+  }
+  return branches;
+};
