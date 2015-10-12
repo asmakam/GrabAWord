59e335eb3421b5d4f882902ee1e5f6b52edbdb05
tekstop <tekstop@outlook.com>
2015-10-11 23:17:49 -0400

    Should work

--- GrabAWord/server/BoardMethods.jsx
+++ GrabAWord/server/BoardMethods.jsx
@@ -1,107 +1,130 @@
 Meteor.methods({
 
-	// Function creates the Board and returns the id
-	createBoard() {
-		// Need to create random tiles 4 x 5
-		var tiles = createTiles();
-		const board_id = Boards.insert({ createdAt: new Date(), results: [], tiles: tiles});
-		return board_id;
-	},
+  // Function creates the Board and returns the id
+  createBoard() {
+      // Need to create random tiles 4 x 5
+      var tiles = createTiles();
+      const board_id = Boards.insert({
+        createdAt: new Date(),
+        results: [],
+        tiles: tiles
+      });
+      return board_id;
+    },
 
-	// Someone grabs a words
-	grabWord(word, board, user) {
-		//If the word is walid,
-		if (Words.findOne({key: word })) {
-			// Is the word in BoardWords
-			if (!BoardWords.findOne({boardId:board, word: word})) {
-				var bwId = BoardWords.insert({boardId: board, word: word, user: user, points: 0});
-				// Calculate points and update the word
-				Meteor.defer(() => {
-					Meteor.call('updateWordsWithPoints',bwId, word);
-				});
-				return 'success';
-			}else{
-				return 'beaten';
-			}
-		} else{
-				return 'incorrect';
-		}
+    // Someone grabs a words
+    grabWord(word, board, user) {
+      //If the word is walid,
+      if (Words.findOne({
+          key: word
+        })) {
+        // Is the word in BoardWords
+        if (!BoardWords.findOne({
+            boardId: board,
+            word: word
+          })) {
+          var bwId = BoardWords.insert({
+            boardId: board,
+            word: word,
+            user: user,
+            points: 0
+          });
+          // Calculate points and update the word
+          Meteor.defer(() => {
+            Meteor.call('updateWordsWithPoints', bwId, word);
+          });
+          return 'success';
+        } else {
+          return 'beaten';
+        }
+      } else {
+        return 'incorrect';
+      }
 
-	},
+    },
 
-	updateWordsWithPoints(wordId, word) {
-		var LettersAndPoints = [];
-		LettersAndPoints['A'] = 1;
-		LettersAndPoints['B'] = 3;
-		LettersAndPoints['C'] = 3;
-		LettersAndPoints['D'] = 2;
-		LettersAndPoints['E'] = 1;
-		LettersAndPoints['F'] = 4;
-		LettersAndPoints['G'] = 2;
-		LettersAndPoints['H'] = 4;
-		LettersAndPoints['I'] = 1;
-		LettersAndPoints['J'] = 8;
-		LettersAndPoints['K'] = 5;
-		LettersAndPoints['L'] = 1;
-		LettersAndPoints['M'] = 3;
-		LettersAndPoints['N'] = 1;
-		LettersAndPoints['O'] = 1;
-		LettersAndPoints['P'] = 3;
-		LettersAndPoints['Q'] = 10;
-		LettersAndPoints['R'] = 1;
-		LettersAndPoints['S'] = 1;
-		LettersAndPoints['T'] = 1;
-		LettersAndPoints['U'] = 1;
-		LettersAndPoints['V'] = 4;
-		LettersAndPoints['W'] = 4;
-		LettersAndPoints['X'] = 8;
-		LettersAndPoints['Y'] = 4;
-		LettersAndPoints['Z'] = 10;
+    updateWordsWithPoints(wordId, word) {
+      var LettersAndPoints = [];
+      LettersAndPoints['A'] = 1;
+      LettersAndPoints['B'] = 3;
+      LettersAndPoints['C'] = 3;
+      LettersAndPoints['D'] = 2;
+      LettersAndPoints['E'] = 1;
+      LettersAndPoints['F'] = 4;
+      LettersAndPoints['G'] = 2;
+      LettersAndPoints['H'] = 4;
+      LettersAndPoints['I'] = 1;
+      LettersAndPoints['J'] = 8;
+      LettersAndPoints['K'] = 5;
+      LettersAndPoints['L'] = 1;
+      LettersAndPoints['M'] = 3;
+      LettersAndPoints['N'] = 1;
+      LettersAndPoints['O'] = 1;
+      LettersAndPoints['P'] = 3;
+      LettersAndPoints['Q'] = 10;
+      LettersAndPoints['R'] = 1;
+      LettersAndPoints['S'] = 1;
+      LettersAndPoints['T'] = 1;
+      LettersAndPoints['U'] = 1;
+      LettersAndPoints['V'] = 4;
+      LettersAndPoints['W'] = 4;
+      LettersAndPoints['X'] = 8;
+      LettersAndPoints['Y'] = 4;
+      LettersAndPoints['Z'] = 10;
 
-		let score = 0;
-		word.split("").forEach(l => {
-			score += LettersAndPoints[l];
-		});
-		BoardWords.update({_id:wordId}, {$set: { points: score}});
-	},
+      let score = 0;
+      word.split("").forEach(l => {
+        score += LettersAndPoints[l];
+      });
+      BoardWords.update({
+        _id: wordId
+      }, {
+        $set: {
+          points: score
+        }
+      });
+    },
 
-	leaderboardResults() { 
-		var board = Boards.findOne({}, {
-				    sort: {
-				      createdAt: -1
-				    },
-				    limit: 1
-				  });
-		console.log(board);
-		var res =  BoardWords.aggregate([{
-		    $match: {
-		      boardId: board._id
-		    }
-		  }, {
-		    $group: {
-		      _id: '$user',
-		      totalPoints: {
-		        $sum: '$points'
-		      }
-		    }
-  		}
-  		]);
-	 if (board.results === []) {
-	 	console.log(res);
-	 	Boards.update({_id:board._id}, {$set: { results: res}});
-	 }
+    leaderboardResults() {
+      var board = Boards.findOne({}, {
+        sort: {
+          createdAt: -1
+        },
+        limit: 1
+      });
+      if (board.results) {
+        var res = BoardWords.aggregate([{
+          $match: {
+            boardId: board._id
+          }
+        }, {
+          $group: {
+            _id: '$user',
+            totalPoints: {
+              $sum: '$points'
+            }
+          }
+        }]);
+        Boards.update({
+          _id: board._id
+        }, {
+          $set: {
+            results: res
+          }
+        });
+      }
 
-  	}
+    }
 
 });
 
 
 var createTiles = function() {
-	const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	let text = "";
-	for( var i=0; i < 20; i++ ) // TODO use constants for board size
-				//TODO - use char probability
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let text = "";
+  for (var i = 0; i < 20; i++) // TODO use constants for board size
+  //TODO - use char probability
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-    return text;
+  return text;
 };
